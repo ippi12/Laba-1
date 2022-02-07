5a047405ce3b9bfbca00e9aff314911a225ec5bf
ippi12 <98397386+ippi12@users.noreply.github.com>
2022-02-07 14:58:43 +0300

    Add files via upload

---
+++ Authentification/Cells/EmailTableViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  EmailTableViewCell.swift
+//  Authentification
+//
+//  Created by Компьютер on 07.02.2022.
+//
+
+import UIKit
+
+class EmailTableViewCell: UITableViewCell {
+
+    var loginField: UITextField = {
+       let textField = UITextField()
+        textField.clearButtonMode = .whileEditing
+        textField.placeholder = "Введите e-mail"
+        textField.textAlignment = .center
+        textField.layer.cornerRadius = 10
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.backgroundColor = .systemGray6
+        return textField
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        selectionStyle = .none
+        //loginView.translatesAutoresizingMaskIntoConstraints = false
+//            loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        loginField.translatesAutoresizingMaskIntoConstraints = false
+
+        //contentView.addSubview(loginView)
+//            contentView.addSubview(loginLabel)
+        contentView.addSubview(loginField)
+        NSLayoutConstraint.activate([
+            /*loginView.heightAnchor.constraint(equalToConstant: 50),
+            loginView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            loginView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0),
+            loginView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 110),
+            loginView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),*/
+            
+//                loginLabel.heightAnchor.constraint(equalToConstant: 50),
+//                loginLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+//                loginLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0),
+//                loginLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+//                loginLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -200),
+//
+            loginField.heightAnchor.constraint(equalToConstant: 50),
+            loginField.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            loginField.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0),
+            loginField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            loginField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0)])
+    }
+}
