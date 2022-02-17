a8fa72dd1fd934f8373368c9f818778fd5a5e79f
ippi12 <98397386+ippi12@users.noreply.github.com>
2022-02-17 10:45:36 +0300

    Add files via upload

---
+++ Authentification (последня итерация)/Cells/CustomHeader.swift
@@ -0,0 +1,19 @@
+//
+//  CustomHeader.swift
+//  Authentification
+//
+//  Created by Компьютер on 09.02.2022.
+//
+
+import UIKit
+
+class CustomHeader: UITableViewHeaderFooterView {
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
